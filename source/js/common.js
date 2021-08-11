9d063173cd58b6e4dcff5d3529870674c4010dfc
Andrey <soinproduction@gmail.com>
2021-08-11 13:00:53 +0300

    second push

--- source/js/common.js
+++ source/js/common.js
@@ -2,17 +2,166 @@
 
   const Sliders = {
     MAIN_SLIDER: {
-      ELEMENT: $(".main-slider"),
+      ELEMENT: $(".first-slider"),
       SETTINGS: {
         accessibility: true,
         arrows: true,
-        prevArrow: $(".prev-arrow"),
-        nextArrow: $(".next-arrow"),
+        prevArrow: $(".prev-arrow1"),
+        nextArrow: $(".next-arrow1"),
         dots: false,
         speed: 750,
         slidesToShow: 1,
         infinite: true,
         fade: true,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              centerPadding: ("40px"),
+              initialSlide: 2,
+            },
+          },
+        ],
+      },
+      // BREAKPOINT: 1600,
+      // CLASSNAME: '',
+    },
+
+    SECOND_SLIDER: {
+      ELEMENT: $(".second-slider"),
+      SETTINGS: {
+        accessibility: true,
+        arrows: true,
+        prevArrow: $(".prev-arrow2"),
+        nextArrow: $(".next-arrow2"),
+        dots: false,
+        speed: 750,
+        slidesToShow: 1,
+        infinite: true,
+        fade: true,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              centerPadding: ("40px"),
+              initialSlide: 2,
+            },
+          },
+        ],
+      },
+      // BREAKPOINT: 1600,
+      // CLASSNAME: '',
+    },
+
+    HARD_SLIDER: {
+      ELEMENT: $(".hard-slider"),
+      SETTINGS: {
+        accessibility: true,
+        arrows: true,
+        prevArrow: $(".prev-arrow4"),
+        nextArrow: $(".next-arrow4"),
+        dots: false,
+        speed: 750,
+        slidesToShow: 1,
+        infinite: true,
+        fade: true,
+        asNavFor: $(".nav-second"),
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              centerPadding: ("40px"),
+              initialSlide: 2,
+            },
+          },
+        ],
+      },
+      // BREAKPOINT: 1600,
+      // CLASSNAME: '',
+    },
+
+    NAV_SLIDER: {
+      ELEMENT: $(".nav-second"),
+      SETTINGS: {
+        accessibility: true,
+        asNavFor: $(".hard-slider"),
+        arrows: true,
+        prevArrow: $(".prev-nav"),
+        nextArrow: $(".next-nav"),
+        dots: false,
+        focusOnSelect: true,
+        speed: 750,
+        slidesToShow: 5,
+        infinite: true,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              centerPadding: ("40px"),
+              initialSlide: 2,
+            },
+          },
+        ],
+      },
+      // BREAKPOINT: 1600,
+      // CLASSNAME: '',
+    },
+
+    CATALOG_SLIDER: {
+      ELEMENT: $(".catalog-slider"),
+      SETTINGS: {
+        accessibility: true,
+        arrows: true,
+        prevArrow: $(".prev-arrow3"),
+        nextArrow: $(".next-arrow3"),
+        dots: false,
+        speed: 750,
+        slidesToShow: 1,
+        infinite: true,
+        fade: true,
+        asNavFor: $(".nav-first"),
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              centerPadding: ("40px"),
+              initialSlide: 2,
+            },
+          },
+        ],
+      },
+      // BREAKPOINT: 1600,
+      // CLASSNAME: '',
+    },
+
+    NAV_SLIDER_FIRST: {
+      ELEMENT: $(".nav-first"),
+      SETTINGS: {
+        accessibility: true,
+        asNavFor: $(".catalog-slider"),
+        arrows: true,
+        prevArrow: $(".prev-nav2"),
+        nextArrow: $(".next-nav2"),
+        dots: false,
+        focusOnSelect: true,
+        speed: 750,
+        slidesToShow: 6,
+        infinite: true,
         responsive: [
           {
             breakpoint: 992,
@@ -59,6 +208,11 @@
   }
 
   initialazeSlickSlider(Sliders.MAIN_SLIDER);
+  initialazeSlickSlider(Sliders.SECOND_SLIDER);
+  initialazeSlickSlider(Sliders.HARD_SLIDER);
+  initialazeSlickSlider(Sliders.NAV_SLIDER);
+  initialazeSlickSlider(Sliders.CATALOG_SLIDER);
+  initialazeSlickSlider(Sliders.NAV_SLIDER_FIRST);
 
 
 
